bc5d1f5152d9ca062f5999d04b06516bb2a20917
MIGUELANGEL GARAY <miguelangelcdm@gmail.com>
2023-06-24 21:24:16 -0500

    v1.0-min

--- App.jsx
+++ App.jsx
@@ -1,32 +1,54 @@
+/* eslint-disable react/jsx-key */
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
+import cardmockdata from "./cardmockdata";
 
-function App() {
+export default function App() {
+  const cardelements = cardmockdata.map((data) => {
+    return (
+      <Card
+        id={data.id}
+        // img={data.img}
+        // rating={data.rating}
+        // reviewCount={data.reviewCount}
+        // country={data.country}
+        // title={data.title}
+        // price={data.price}
+        // openSpots={data.openSpots}
+        // online={data.online}
+        {...data}
+      />
+    );
+  });
+
   return (
     <div>
       <Navbar />
       <Hero />
-      <Card
-        img={cards[0].img}
-        rating={cards[0].rating}
-        reviewCount={cards[0].reviewCount}
-        country={cards[0].country}
-        title={cards[0].title}
-        price={cards[0].price}
-      />
+      <div className="cards-list">{cardelements}</div>
     </div>
   );
 }
-export default App;
+// export default App;
 
-const cards = [
-  {
-    img: "2508B072-43BD-4CBA-81B4-A1C1296DF6DE.jpeg",
-    rating: "5.0",
-    reviewCount: 6,
-    country: "Peru",
-    title: "Life Lessons with Michael Gray",
-    price: 2000,
-  },
-];
+// const cards = [
+//   {
+//     img: "2508B072-43BD-4CBA-81B4-A1C1296DF6DE.jpeg",
+//     rating: "5.0",
+//     reviewCount: 6,
+//     country: "Peru",
+//     title: "Life Lessons with Michael Gray",
+//     price: 2000,
+//   },
+// ];
+// console.log(cards[0].img)
+
+// const names=["miguel","leslie","angelica","nicol","christian"]
+// const capitalized=names.map((name)=>{
+//   return name[0].toUpperCase()+name.slice(1)
+// })
+// console.log(capitalized)
+// const cities=["El Cairo","Birminghan","Jakarta"]
+// const paragraphs=cities.map(city=>`<p>${city}</p>`)
+// console.log(paragraphs)
--- components/Card.jsx
+++ components/Card.jsx
@@ -1,11 +1,23 @@
 export default function card(props) {
+  let badgeText;
+  let additionalStyles = {};
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+    additionalStyles.backgroundColor = "red";
+    additionalStyles.color = "white";
+  } else if (props.online) {
+    badgeText = "ONLINE";
+    additionalStyles.backgroundColor = "green";
+    additionalStyles.color = "white";
+  }
   return (
     <div className="card">
+      {badgeText && <div style={additionalStyles} className="card--badge">{badgeText}</div>}
       <img src={`/images/${props.img}`} className="card--image" />
       <div className="card--stats">
         <img
           src="/images/[CITYPNG.COM]HD Outline Red Star Transparent PNG - 8192x7790.png"
-          width={"25px"}
+          width={"20px"}
         />
         <span>{props.rating}</span>
         <span className="gray">({props.reviewCount}) • </span>
